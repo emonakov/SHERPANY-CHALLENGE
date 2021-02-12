54985e563db0177826c8414ae461fa0b91b00a68
Eugene Monakov <evgenii.monakov@auto1.com>
2021-02-12 11:46:02 +0100

    user card components

---
+++ src/components/UserCard.tsx
@@ -0,0 +1,36 @@
+import { FC } from 'react';
+import styled from 'styled-components';
+import { Box, Text, Button, Avatar, Card } from 'grommet';
+
+import { UserInterface } from '../StateInterface';
+
+const ColoredText = styled(Text)`
+  color: #746a32;
+  font-size: 0.9rem;
+`;
+
+const UserCard: FC<{ user: UserInterface }> = ({ user }) => (
+  <Box
+    pad="large"
+    align="center"
+    background={{ color: 'light-2', opacity: 'strong' }}
+    round
+    gap="small"
+  >
+    <Avatar src={user.picture.thumbnail} />
+    <Text>
+      {user.name.title} {user.name.first} {user.name.last}
+    </Text>
+    <Card
+      background={{ color: 'light-3' }}
+      pad={{ vertical: 'small', horizontal: 'medium' }}
+      justify="center"
+    >
+      <ColoredText>{user.login.username}</ColoredText>
+      <ColoredText>{user.email}</ColoredText>
+    </Card>
+    <Button label="Open" />
+  </Box>
+);
+
+export default UserCard;
---
+++ src/components/UserList.tsx
@@ -0,0 +1,36 @@
+import { FC, useContext } from 'react';
+import styled from 'styled-components';
+import { Box, Grid, ResponsiveContext } from 'grommet';
+
+import UserCard from './UserCard';
+import { useUsersQuery } from '../hooks/useUsersQuery';
+
+const BoxRelative = styled(Box)`
+  position: relative;
+`;
+
+const UserList: FC = () => {
+  const { data, Interceptor } = useUsersQuery({ nat: 'ES' });
+  const size = useContext(ResponsiveContext);
+
+  return data ? (
+    <BoxRelative
+      direction="column"
+      justify="center"
+      pad="large"
+      background="dark-1"
+      gap="medium"
+    >
+      <Grid columns={size !== 'small' ? 'medium' : '100%'} gap="large">
+        {data.map((page) =>
+          page.results.map((user) => (
+            <UserCard key={user.id.value} user={user} />
+          )),
+        )}
+      </Grid>
+      {Interceptor}
+    </BoxRelative>
+  ) : null;
+};
+
+export default UserList;
